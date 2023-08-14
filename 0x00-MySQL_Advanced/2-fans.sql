2d291a432220bd0dd9bb761daa71b33a1ba2745a
Clive Sasaka <clivesasaka@gmail.com>
2023-08-14 12:11:36 +0300

    Fix the counting of fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,3 @@
 --  script that ranks country origins of bands, ordered by the number of (non-unique) fans
 --  that have the band in their top 10.
-SELECT origin, COUNT(*) AS num_fans FROM metal_bands GROUP BY origin ORDER BY num_fans DESC;
+SELECT origin, SUM(fans) AS num_fans FROM metal_bands GROUP BY origin ORDER BY num_fans DESC;
